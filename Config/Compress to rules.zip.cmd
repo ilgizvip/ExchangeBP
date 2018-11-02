f936703a17f7272cbd64584feb87927b82686133
Ilgiz <ilgizvip@gmail.com>
2018-11-02 11:45:35 +0500

    Изменен скрипт компрессии

--- Config/Compress to rules.zip.cmd
+++ Config/Compress to rules.zip.cmd
@@ -1,2 +1,2 @@
 chcp 1251
-"C:\Program Files\WinRAR\winrar.exe" a -afzip "D:\YandexDisk\YandexDisk\йд20\Rules.zip" ExchangeRules.xml CorrespondentExchangeRules.xml RegistrationRules.xml 
+"C:\Program Files\WinRAR\winrar.exe" a -afzip "Rules.zip" ExchangeRules.xml CorrespondentExchangeRules.xml RegistrationRules.xml 
